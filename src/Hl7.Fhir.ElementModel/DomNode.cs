d7460d01e343816eeea6b8ce77965d79282c4f4c
George Stoian <george.stoian@endava.com>
2019-07-18 10:11:48 +0300

    test commit

--- src/Hl7.Fhir.ElementModel/DomNode.cs
+++ src/Hl7.Fhir.ElementModel/DomNode.cs
@@ -2,7 +2,7 @@
  * Copyright (c) 2018, Firely (info@fire.ly) and contributors
  * See the file CONTRIBUTORS for details.
  * 
- * This file is licensed under the BSD 3-Clause license
+ * This file is licensed under the BSD 3-Clause license 
  * available at https://raw.githubusercontent.com/ewoutkramer/fhir-net-api/master/LICENSE
  */
 
